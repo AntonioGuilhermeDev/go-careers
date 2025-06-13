d30537d92a8b45ad0cdde05620e1d7ca17672802
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-06-13 08:16:58 -0300

    Ajustando versoes das dependencias

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <MainNav/>
+  <main-nav />
 </template>
 
 <script>
@@ -10,6 +10,5 @@ export default {
   components: {
     MainNav,
   },
-}
-
+};
 </script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,13 +1,22 @@
-<script>
-export default {
-  name: "MainNav"
-}
-</script>
-
 <template>
-  <header class="w-full text-sm">Main Nav</header>
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
+        <a href="/" class="flex h-full item-center text-xl mt-4">{{ company }}</a>
+        <h2 class="ml-2 flex h-full items-center text-xs">Developed by {{ author }}</h2>
+      </div>
+    </div>
+  </header>
 </template>
 
-<style scoped>
-
-</style>
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "GO Careers",
+      author: "GO Careers Team",
+    };
+  },
+};
+</script>
