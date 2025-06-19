8005fc7495764ad8e48bf3ffde24c0414b72964b
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-06-19 17:34:33 -0300

    Testes mainav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,8 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full item-center text-xl mt-4">{{ company }}</a>
-        <h2 class="ml-2 flex h-full items-center text-xs">Developed by {{ author }}</h2>
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
@@ -14,8 +13,7 @@ export default {
   name: "MainNav",
   data() {
     return {
-      company: "GO Careers",
-      author: "GO Careers Team",
+      company: "Go Careers",
     };
   },
 };
