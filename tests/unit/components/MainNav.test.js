f97a4cad9596d5e46f3a884d320fa9c844462db7
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-06-22 18:57:12 -0300

    Testando componente

---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,47 @@
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+
+import MainNav from "@/components/MainNav.vue";
+
+describe("MainNav", () => {
+  it("displays company name", () => {
+    render(MainNav);
+    const companyName = screen.getByText("Go Careers");
+    expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent);
+    expect(navigationMenuTexts).toEqual([
+      "Times",
+      "Localização",
+      "Vida na Go Careers",
+      "Como nós recrutamos",
+      "Estudantes",
+      "Empregos",
+    ]);
+  });
+
+  describe("when the user logs in", () => {
+    it("displays user profile picture", async () => {
+      render(MainNav);
+
+      let profileImage = screen.queryByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).not.toBeInTheDocument();
+
+      const loginButton = screen.getByRole("button", {
+        name: /sign in/i,
+      });
+      await userEvent.click(loginButton);
+
+      profileImage = screen.getByRole("img", {
+        name: /user profile image/i,
+      });
+      expect(profileImage).toBeInTheDocument();
+    });
+  });
+});
