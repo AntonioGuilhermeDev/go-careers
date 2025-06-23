6a09bbd4f0b5c13e9a2d07ef0fc4f87ba2a163e9
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-06-23 10:04:46 -0300

    Implementando icone e texto da subnav

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="w-full h-10 border-b border-solid border-brand-gray-1 bg-white">
-    <div class="flex items-center h-full px-8">THis is a subnav</div>
+    <div class="flex items-center h-full px-8">
+      <div v-if="onJobResultsPage">
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+        <span><span class="text-brand-green-1">1653 </span><span>vagas relacionadas</span></span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "TheSubnav",
+  data() {
+    return {
+      onJobResultsPage: true,
+    };
+  },
 };
 </script>
