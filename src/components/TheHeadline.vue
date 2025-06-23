94527378cf32f5dcf7f19eea4771a520601d1e49
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-06-23 11:50:12 -0300

    Adicionando componente da headline

---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Construido para todos</h1>
+    <h2>Encontre seu próximo emprego na Go Careers</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHeadline",
+  data() {
+    return {
+      sample: "Hello",
+    };
+  },
+  created() {
+    console.log("Hey, I've been created. My data exists. Look: ", this.sample);
+  },
+};
+</script>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -4,7 +4,9 @@
       <div class="grid grid-cols-12">
         <div class="col-start-1 col-span-1 border border-blue-700">1 coluna</div>
 
-        <div class="col-start-2 col-span-5 border border-blue-700">5 coluna</div>
+        <div class="col-start-2 col-span-5 border border-blue-700">
+          <the-headline />
+        </div>
 
         <div class="col-start-7 col-span-5 border border-blue-700">5 coluna</div>
 
@@ -15,7 +17,12 @@
 </template>
 
 <script>
+import TheHeadline from "@/components/TheHeadline.vue";
+
 export default {
   name: "TheHero",
+  components: {
+    TheHeadline,
+  },
 };
 </script>
