19d2479ed4f895a1db743e3f0a4e848c6c724ec3
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-06-24 17:32:57 -0300

    Adicionando componente JobSearchForm

---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <form>Form</form>
+</template>
+
+<script>
+export default {
+  name: "JobSearchForm",
+};
+</script>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,16 +1,17 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
+    <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-start-1 col-span-1 border border-blue-700">1 coluna</div>
+        <div class="col-start-1 col-span-1"></div>
 
-        <div class="col-start-2 col-span-5 border border-blue-700">
+        <div class="col-start-2 col-span-5">
           <the-headline />
+          <JobSearchForm />
         </div>
 
-        <div class="col-start-7 col-span-5 border border-blue-700">5 coluna</div>
+        <div class="col-start-7 col-span-5"></div>
 
-        <div class="col-start-12 col-span-1 border border-blue-700">1 coluna</div>
+        <div class="col-start-12 col-span-1"></div>
       </div>
     </section>
   </main>
@@ -18,11 +19,13 @@
 
 <script>
 import TheHeadline from "@/components/TheHeadline.vue";
+import JobSearchForm from "@/components/JobSearchForm.vue";
 
 export default {
   name: "TheHero",
   components: {
     TheHeadline,
+    JobSearchForm,
   },
 };
 </script>
